18791e8b9d6b646708dcc4a1b2a62b84d7bdef24
Apoorv Gupta <145976905+apoorv2766@users.noreply.github.com>
2026-05-26 12:50:12 +0530

    first commit

---
+++ agent.js
@@ -0,0 +1,53 @@
+import { ChatGroq } from "@langchain/groq"
+import { createReactAgent } from "@langchain/langgraph/prebuilt"
+import "dotenv/config"
+import { TavilySearch } from "@langchain/tavily";
+import { tool } from "langchain";
+import z from "zod"
+
+
+async function main() {
+    const model = new ChatGroq({
+        model: "openai/gpt-oss-120b",
+        temperature: 0,
+    })
+    const search = new TavilySearch({
+        maxResults: 5,
+        topic: "general",
+    });
+
+    const calendatEvents = tool(
+        async ({ query }) => {
+            //google calendar logic goes here
+            return JSON.stringify([{
+                title: "meeting with joy",
+                time: "2 PM",
+                location: "Gmeet"
+            }])
+        },
+        {
+            name: "get-calendar-events",
+            description: "call to get calendar events",
+            schema: z.object({
+                query: z.string().describe("the query to use in calendar event search")
+            })
+        }
+    )
+
+    const agent = createReactAgent({
+        llm: model,
+        tools: [search, calendatEvents]
+    })
+
+    const result = await agent.invoke({
+        messages: [
+            {
+                role: "human",
+                content: "hi do i have any meeting?"
+            }
+        ]
+    })
+    console.log("Assistant:", result)
+}
+
+main()
